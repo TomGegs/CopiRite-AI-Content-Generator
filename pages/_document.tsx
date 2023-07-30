6efa02b38e3b97cbfa8ae2b13fdf3272f2e597af
TomG <tomgegs@outlook.com>
2023-07-31 09:44:18 +1000

    Updated metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+        {/* Add meta tags here */}
+        <meta
+          name="Copi.Rite"
+          content="A.I. generated marketing copy for your product or service."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
-import { Inter } from "next/font/google";
 import FormContainer from "./components/FormContainer";
 
-const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
-    title: "Qwicopy",
-    description: "Ai generated copywriting for your business",
+    title: "Copi.Rite",
+    description: "A.I. generated marketing copy for your product or service.",
 };
 
 export default function Home() {
     return (
         <div className="min-h-[100dvh] w-screen flex flex-col">
             <div className="relative flex flex-grow flex-col">
-                {/* <nav></nav> */}
                 <main className="flex-1 mx-auto">
                     <FormContainer />
                 </main>
